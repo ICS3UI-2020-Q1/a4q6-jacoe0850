aaf9fb1abd1f28b0793f11dbffcc906c72edf8fd
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-06 07:53:51 +0000

    Finished prime nubmers

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * finds out if a number is prime
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+    // ask for integer
+    System.out.println("Which number would you like to check?");
+    int num = input.nextInt();
+    int counter = 2;
+    while(counter < num){
+      if(num % counter == 0){
+        System.out.println(num + " is not a prime number");
+        break;
+      }
+      counter = counter + 1;
+    }
+    if(num == counter){
+      System.out.println(num + " is a prime number");
+  }
   }
 }
